382613ec5cada65cd29690b3f30fe35c2d5e6b75
mrc-sprinklr <mayukh.raychaudhuri@sprinklr.com>
2022-06-05 13:50:38 +0530

    less stun-server

--- app.js
+++ app.js
@@ -129,21 +129,21 @@ const configuration = {
     {
       urls: "stun:stun.l.google.com:19302",
     },
-    {
-      urls: "stun:stun1.l.google.com:19302",
-    },
-    {
-      urls: "stun:stun2.l.google.com:19302",
-    },
-    {
-      urls: "stun:stun3.l.google.com:19302",
-    },
-    {
-      urls: "stun:stun4.l.google.com:19302",
-    },
-    {
-      urls: "stun:stun.stunprotocol.org",
-    },
+    // {
+    //   urls: "stun:stun1.l.google.com:19302",
+    // },
+    // {
+    //   urls: "stun:stun2.l.google.com:19302",
+    // },
+    // {
+    //   urls: "stun:stun3.l.google.com:19302",
+    // },
+    // {
+    //   urls: "stun:stun4.l.google.com:19302",
+    // },
+    // {
+    //   urls: "stun:stun.stunprotocol.org",
+    // },
     // {
     //   urls: "turn:openrelay.metered.ca:80",
     //   username: "openrelayproject",
